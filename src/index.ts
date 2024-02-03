ea4dbce31dce553a0ec17c1b7fd739337e1256ac
Jasid Martinez <jasidmartinez21@gmail.com>
2024-02-03 15:18:08 -0500

    vercel.json file added

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,24 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import PaymentRouter from './routes/Payment.routes';
 import homeRouter from './routes/Home.routes';
+import * as path from 'path';
 
 dotenv.config();
 
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors({ origin: '*', methods: ['GET', 'POST'] }));
+app.use(express.static(path.join(__dirname, '..', 'dist')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/home', homeRouter);
 app.use('/payment', PaymentRouter);
+
+app.get('*', (req: Request, resp: Response) => {
+  console.log(__dirname);
+  resp.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log("Listening in port: ", PORT);
